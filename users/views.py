e0035e12174984265b21587d3b48c2097b4f4a7b
Olivia Erni <oliviaerni@MacBook-Pro.local>
2026-04-07 13:33:58 +1000

    Login/Registration

--- users/views.py
+++ users/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import UserRegistrationForm, AuthenticationForm
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            next_url = request.GET.get('next', reverse("main:home"))
-            return HttpResponseRedirect(next_url)
-        else:
-            messages.error(request, "Invalid Credentials.")
-    return render(request, "users/login.html")
+            return redirect('main:home')
+    else:
+        form = AuthenticationForm(request)
+    return render(request, 'users/login.html', {'form': form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required(login_url='users:login')
+def user(request):
+    return render(request, "main/home.html")
 
 def logout_view(request):
     logout(request)
